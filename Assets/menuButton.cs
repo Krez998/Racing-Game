d61ab90d80f2c6c855ddf2353f02e87ce74a62c7
Егор Мазков <84621490+EgorMazkov@users.noreply.github.com>
2023-12-29 17:23:52 +0300

    добавление интерфейса

---
+++ Assets/menuButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuButton : MonoBehaviour
+{
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void ExitApp()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
